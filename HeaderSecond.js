b9daff84edcecdcaf68594dbe85091ea42bb428f
prakharsingh123 <102253372+prakharsingh123@users.noreply.github.com>
2023-10-08 01:24:37 +0530

    Create HeaderSecond.js

---
+++ HeaderSecond.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './HeaderSecond.css'
+
+
+
+
+function HeaderSecond() {
+  return (
+    <div id="desktop2">
+        <div className="m-0 p-6 w-[720px] ml-[30%] h-[120px] justify-center relative">
+          <div id='heading' className=" flex mt-[2%] justify-center">How it works ?</div>
+          <div id='subHeading' className="flex justify-center">Renting a luxury car has never been easier. Our streamlined process makes it simple for you to book and confirm your vehicle of choice online</div>
+          </div>
+      <div className=' m-16 p-3 flex justify-around'>
+       <div id='heading-2' className=" m-2 w-[600px] h-[500px]">
+       <div className="m-3 p-1 mt-6 bg-white w-[580px] h-[120px] rounded-sm shadow-lg shadow-grey ">
+        <img className="w-10 h-10 m-6" src='https://imgs.search.brave.com/BGKYz5mdv6ccfbWQGU20TuZdrZupuOr9zG2aWX489L4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zbWFs/bGltZy5wbmdrZXku/Y29tL3BuZy9zbWFs/bC80MzctNDM3MDQw/MV9sb2NhdGlvbi1p/Y29uLW5ldy1sb2Nh/dGlvbi1pY29uLXBu/Zy5wbmc' alt='location'></img>
+        <div id='location-1' className="-mt-[13%] -ml-[20%]">Choose your location</div>
+        <div id='location-div' className="ml-[21%]">Choose from our wide range of premium cars, select the pickup and return dates and locations that suit you best. </div>
+       </div>
+       <div className="m-3 p-1 mt-6 bg-white w-[580px] h-[120px] rounded-sm shadow-lg shadow-grey">
+        <img className="w-10 h-10 m-6" src='https://imgs.search.brave.com/FyaGPNaZvB1mw8bR2Ng2sKwShZZEwppXnc6qg6c-UY4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxNi8x/MC9DYWxlbmRhci5w/bmc' alt='location'></img>
+        <div id='location-1' className="-mt-[13%] -ml-[25%]">Book and Confirm</div>
+        <div id='location-div' className="ml-[21%]">Book your desired car with just a few clicks and receive an instant confirmation via email or SMS.</div>
+       </div>
+       <div className="m-3 p-1 mt-6 bg-white w-[580px] h-[120px] rounded-sm shadow-lg shadow-grey">
+        <img className="w-10 h-10 m-6" src='https://imgs.search.brave.com/xXLK7Uz6QZ6gXHlhghbW4ogOkxjUkZJMkJ3Z6n0g4sM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2l0eXBuZy5jb20v/cHVibGljL3VwbG9h/ZHMvcHJldmlldy9o/ZC1yZWQtZG91Ymxl/LXRpY2stY2hlY2st/bWFyay1pY29uLXBu/Zy0zMTYyMzM0OTUw/NnhsM3c2eXJjZ3gu/cG5n' alt='location'></img>
+        <div id='location-1' className="-mt-[12%] -ml-[30%]">Enjoy your ride</div>
+        <div id='location-div' className="ml-[21%]">Pick up your car at the designated location and enjoy your premium driving experience with our top-quality service.</div>
+       </div>
+       
+       </div>
+        <div id='heading-3'  className="m-2 w-[600px] h-[450px]">
+        <div id='car-2' className="absolute flex justify-center m-4 mt-[10%] w-[550px] h-[300px]"><img src='https://imgs.search.brave.com/4rMg2NigzqC8kjk04Tf-hD00H06uQeZyWmp5ztFErPs/rs:fit:860:0:0/g:ce/aHR0cDovL3BuZ2lt/Zy5jb20vdXBsb2Fk/cy9qZWVwL2plZXBf/UE5HOTkucG5n' alt='car-2'></img></div>
+
+        </div>
+      </div>
+      <div className="absolute w-100px"> 
+
+      </div>
+    </div>
+  )
+}
+
+export default HeaderSecond
